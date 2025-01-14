6d3e1d985b78edf26de72561175d93bb9f4f5c66
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2025-01-14 17:14:49 +0530

    basics_2.py

--- basics_2.py
+++ basics_2.py
@@ -4,3 +4,11 @@ if(num%2==0):
     print("number is even")
 else:
     print("number is odd")
+
+
+#odd-even
+num=int(input("enter a number:"))
+if(num%2==0):
+    print("number is even")
+else:
+    print("number is odd")
