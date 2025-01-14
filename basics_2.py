3f758bdd6c2b7a0d838c1e868f6dd40a52aaa618
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2025-01-14 17:15:13 +0530

    basics_2.py

--- basics_2.py
+++ basics_2.py
@@ -12,3 +12,10 @@ if(num%2==0):
     print("number is even")
 else:
     print("number is odd")
+
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+if(num1>num2):
+    print("largest is:",num1)
+else :
+    print("largest is:",num2)
