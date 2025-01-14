7a6bec960a06740da81668715d9bc3c461d85ed5
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2025-01-14 17:13:52 +0530

    basics.py

---
+++ basics.py
@@ -0,0 +1,65 @@
+print("hello world")      
+
+print("welcome to python class")
+
+name="bhumika bahuguna"
+age=18
+print(type(name))
+print(type(age))
+
+name="bhumika bahuguna"
+age=18
+print("my name is",name,"age is" , age)
+
+name=input()
+age=input()
+print("my name is",name,"age is" , age)
+
+name=input("enter your name:")
+age=input("input your age:")
+print("my name is",name,"age is" , age)
+
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+result=num1*num2
+print("result is:",result)
+
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+add=num1+num2
+sub=num1-num2
+div=num1/num2
+print("addition is:",add)
+print("subtraction is:",sub)
+print("division is:",div)
+
+result=10**5
+print(result)
+
+division=10/3
+print("division result is:",division)
+flooring=10//3
+print("flooring result is:",flooring)
+
+
+num=int(input("enter a number:"))
+result=num**0.5
+print("squareroot of num is:",result)
+
+radius=int(input("enter radius:"))
+area=3.14*radius*radius
+print("area of circle is:",area)
+
+side=int(input("enter side:"))
+area=side*side
+print("area of square is:",area)
+
+l=int(input("enter length:"))
+b=int(input("enter breadth:"))
+area=l*b
+print("area of rectangle is:",area)
+
+c=float(input("enter temperature in celcius:"))
+f=c*1.8+32
+print("temperature in fahrenheit is:",f)
+
